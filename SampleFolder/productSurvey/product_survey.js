c7a17abb2a6823db247985c0aa84a5774984faa8
Yakub357 <egamyakub357@gmail.com>
2025-01-30 11:44:50 -0500

    UX feedback added

--- SampleFolder/productSurvey/product_survey.js
+++ SampleFolder/productSurvey/product_survey.js
@@ -10,6 +10,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('userExperience').value;
 
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -18,6 +19,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('UXfeedback').innerHTML = userExperience;
 
     alert('Thank you for you valuable feedback');
     document.getElementById('userInfo').style.display = 'block';
